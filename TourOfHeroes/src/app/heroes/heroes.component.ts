eb4712a7552bd2e0bbaa6ce783c0965869bb0736
Michael Bond <codemonkey8510@icloud.com>
2022-03-28 15:47:59 -0400

    Services done, navigation next

--- TourOfHeroes/src/app/heroes/heroes.component.ts
+++ TourOfHeroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +9,28 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor() {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {
+    this.getHeroes();
+  }
 
   ngOnInit(): void {}
 
+  getHeroes() {
+    this.heroService
+      .getHeroes()
+      .subscribe((fetchedHeroes) => (this.heroes = fetchedHeroes));
+  }
+
   onSelect(hero: Hero): void {
+    this.messageService.add(
+      `You selected Hero with ID of ${hero.id} and name ${hero.name}.`
+    );
     this.selectedHero = hero;
-    console.log(this.selectedHero);
   }
 }
